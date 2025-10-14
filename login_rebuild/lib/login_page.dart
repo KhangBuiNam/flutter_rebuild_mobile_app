4ea0f78d9e2024a07afd28da913fb92f138804af
buinamkhang582004 <86768423+KhangBuiNam@users.noreply.github.com>
2025-10-14 22:22:01 +0700

    home

--- login_rebuild/lib/login_page.dart
+++ login_rebuild/lib/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'home_page.dart';
-import 'my_icons.dart';
+//import 'my_icons.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginPage extends StatefulWidget {
--- login_rebuild/lib/main.dart
+++ login_rebuild/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'login_page.dart';
 //import 'home_page.dart'; // no problem if not used
 //import 'package:shared_preferences/shared_preferences.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -49,15 +50,26 @@ class _SplashScreenState extends State<SplashScreen> {
     return const Scaffold(
       backgroundColor: Colors.black54,
       body: Center(
-        child: Text(
-          'Welcome 👋',
-          style: TextStyle(fontSize: 28, color: Colors.black87),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(FontAwesomeIcons.handshake, size: 48, color: Colors.white),
+            SizedBox(height: 20),
+            Text(
+              'Hệ thống quản lý điện nước 👋',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
 // shared prefences example:https://www.youtube.com/watch?v=NBbQ0A03l4w
 // source: class MyApp stateless Widget: https://www.youtube.com/watch?v=wE7khGHVkYY
 // stateful widget example: https://www.youtube.com/watch?v=AqCMFXEmf3w
